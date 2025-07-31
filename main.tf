0efa0a9cc8a77d1b13c9bb34dd6182fec058542f
Richard <rbiscazzi3530@conestogac.on.ca>
2025-07-30 22:51:28 -0400

    Changed ami instance - 2nd attempt

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "name" {
-  ami = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  ami = "ami-0bcb08446fd149731"
   instance_type = "t2.micro"
   count = var.core_count
   tags = {
